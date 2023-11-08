0073a11433e3422bd7e8faf406c22ae79c943f53
Dan Valentin <danvaly21@gmail.com>
2023-11-08 13:21:09 +0200

    Update script
    - Update the old script and added website icon

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-const batteryItemOne = document.querySelector(".number-1");
-const batteryItemTwo = document.querySelector(".number-2");
-const batteryItemThree = document.querySelector(".number-3");
-const batteryItemFour = document.querySelector(".number-4");
-const delayInMilliseconds = 1000;
-const list = [
-  batteryItemOne,
-  batteryItemTwo,
-  batteryItemThree,
-  batteryItemFour,
-];
+const batteryItems = document.querySelectorAll(".battery-fill");
+const animationDelay = 1000;
 
-const delayA = (delayInms) => {
-  return new Promise((resolve) => setTimeout(resolve, delayInms));
-};
-
-function Main(delay) {
-  setInterval(async () => {
-    for (let index = 0; index < list.length; index++) {
-      const element = list[index];
-      element.style.background = "white";
-    }
-    let delayres = await delayA(1000);
-    for (let index = 0; index < list.length; index++) {
-      const element = list[index];
-      changeBackground(element);
-      let delayres = await delayA(1000);
-    }
-  }, delay);
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-function changeBackground(element) {
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      return;
-    } else {
-      width++;
-      element.style.background =
-        "linear-gradient(90deg, green " + width + "%,white 100%)";
+
+async function runAnimation() {
+  while (true) {
+    for (const item of batteryItems) {
+      item.style.background = "white";
+    }
+    await delay(animationDelay);
+
+    for (const item of batteryItems) {
+      changeBackground(item);
+      await delay(animationDelay);
     }
   }
 }
-Main(10000);
+
+function changeBackground(element) {
+  let width = 0;
+
+  function animate() {
+    width++;
+    if (width <= 100) {
+      element.style.background = `linear-gradient(90deg, green ${width}%, white 100%)`;
+      requestAnimationFrame(animate);
+    }
+  }
+
+  animate();
+}
+
+runAnimation();
